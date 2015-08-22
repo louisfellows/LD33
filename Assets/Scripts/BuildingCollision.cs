53125b123491db76a93acc06be40f56b17c2682d
Louis Fellows <louisfellows@gmail.com>
2015-08-22 18:53:55 +0100

    Map added, initial score code

--- Assets/Scripts/BuildingCollision.cs
+++ Assets/Scripts/BuildingCollision.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuildingCollision : MonoBehaviour {
+public class BuildingCollision : MonoBehaviour
+{
 
-	public GameObject explosion;
-	public GameObject smoke;
+		public GameObject explosion;
+		public GameObject rubble;
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.name == "Plane") {
-			print (col.relativeVelocity.magnitude);
-			if (col.relativeVelocity.magnitude > 3f) {
-				Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
-				Instantiate(smoke, gameObject.transform.position, Quaternion.identity);
-				Destroy(gameObject);
-			}
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{ 
+	
+		}
+
+		void OnCollisionEnter (Collision col)
+		{
+				if (col.relativeVelocity.magnitude > 4f) {
+						Vector3 rubblePos = gameObject.transform.position;
+						rubblePos.y += 0.2f;
+						Instantiate (explosion, rubblePos, Quaternion.identity);
+						Instantiate (rubble, rubblePos, Quaternion.identity);
+						Destroy (gameObject);
+						GameObject.Find("GameStatus").GetComponent<GameStatus>().AddToScore(100);
+				} else if (col.gameObject.name.EndsWith ("TheMonster")) {
+						gameObject.rigidbody.AddForce (Vector3.MoveTowards (gameObject.transform.position, 
+			                                                  col.gameObject.transform.position, 
+			                                                  -1f));
+						print ("KICK");
+				} else {
+						print (col.gameObject.name);
+				}
+		}
 }
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStatus : MonoBehaviour {
+
+	static int score;
+	public GameObject text;
+	public int growthFactor = 500;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void AddToScore(int addScore) {
+		score += addScore;
+		text.GetComponent<GUIText>().text = score.ToString();
+		float scaleAmount = score / growthFactor;
+		//if (scaleAmount < 1){ scaleAmount = 1; }
+		//GameObject.Find("TheMonster").GetComponent<Transform>().localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+	}
+}
