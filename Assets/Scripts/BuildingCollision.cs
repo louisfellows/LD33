e2fe485519eeac1e6e91a14ad7684983e2956fe9
Louis Fellows <louisfellows@gmail.com>
2015-08-22 16:32:42 +0100

    Buildings, Physics, Particles

---
+++ Assets/Scripts/BuildingCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingCollision : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject smoke;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Plane") {
+			print (col.relativeVelocity.magnitude);
+			if (col.relativeVelocity.magnitude > 3f) {
+				Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+				Instantiate(smoke, gameObject.transform.position, Quaternion.identity);
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	public float moveSpeed = 3f;
+	public float turnSpeed = 50f;
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horiz = Input.GetAxis ("Horizontal");
+		float verti = Input.GetAxis ("Vertical");
+
+		verti = verti * moveSpeed;
+		horiz = horiz * turnSpeed;
+
+		transform.Translate(Vector3.right * verti * Time.deltaTime);
+		transform.Rotate(Vector3.up, horiz * Time.deltaTime);
+
+		animator.SetBool("Moving", (horiz + verti != 0));
+
+		if (Input.GetButton("Punch")) {
+			animator.SetTrigger("Punch");
+		} else if (Input.GetButton ("Kick")) {
+			animator.SetTrigger("Kick");
+		} else if (Input.GetButton("Uppercut")) {
+			animator.SetTrigger("Uppercut");
+		}
+	}
+}
